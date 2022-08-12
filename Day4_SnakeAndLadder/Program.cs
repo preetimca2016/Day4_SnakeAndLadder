9bd7549028857d12e9bf15ab230c90d2bb50f167
“preetimca2016” <preetimca2016@gmail.com>
2022-08-13 01:23:59 +0530

    Program.cs

---
+++ Day4_SnakeAndLadder/Program.cs
@@ -0,0 +1,31 @@
+﻿using Day4_SnakeAndLadder;
+using Day4_SnakeAndLedder;
+using System;
+
+namespace Day4_SnakeAndLedder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & ladder Game");
+            Console.WriteLine("1 for SinglePlayer");
+            Console.WriteLine("2 For Multiplayers");
+
+            int turn = Convert.ToInt32(Console.ReadLine());
+
+            switch (turn)
+            {
+
+                case 1:
+                    FirstPlayer singlePlayer = new FirstPlayer();
+                    singlePlayer.Player1();
+                    break;
+                case 2:
+                    SecondPlayer multiplayer = new SecondPlayer();
+                    multiplayer.Player2();
+                    break;
+            }
+        }
+    }
+}
